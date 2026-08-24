48cc9dc493d3de293ff57579a215ec2e49de06a8
revscalesystemsuy <revscalesystemsuy@gmail.com>
2026-08-24 14:27:02 -0300

    Shift demo foundation to warm cream palette

--- app/demo/layout.tsx
+++ app/demo/layout.tsx
@@ -3,7 +3,7 @@ import { DemoSidebar } from "./demo-sidebar"
 
 export default function DemoLayout({ children }: { children: ReactNode }) {
   return (
-    <div className="min-h-screen bg-[#13120f] text-[#f5efe4] lg:flex">
+    <div className="min-h-screen bg-[#eee5d7] text-[#292722] lg:flex">
       <DemoSidebar />
       <main className="min-w-0 flex-1 pt-14 lg:pt-0">{children}</main>
     </div>
